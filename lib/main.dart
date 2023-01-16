ac3a207c131f63869de94736d7daa8a2a900ac39
Vijay R <vijaythevirtue@gmail.com>
2023-01-16 18:55:54 +0530

    refactor

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Notifications',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
